3af09ea2ff72020d30157fca885aca6b46f9c127
Manny Navarro <manny.navarro@gmail.com>
2016-03-31 11:03:21 -0600

    added contact page with express routes to support real-time submission

--- express_website/app.js
+++ express_website/app.js
@@ -9,10 +9,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Routes
 app.get('/', function(req, res){
     //Message will only be viewed in the console
     console.log('Hello Man!');
@@ -25,6 +27,41 @@ app.get('/about', function(req, res){
     console.log('Hello Man-About!');
     //If you are trying to send it to the client
     res.render('about');
+});
+app.get('/contact', function(req, res){
+    //Message will only be viewed in the console
+    console.log('Hey! It\'s the contact page');
+    //If you are trying to send it to the client
+    res.render('contact');
+});
+
+app.post('/contact/send', function(req, res){
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'manny.navarro@gmail.com',
+            pass: '$$$$MAv1457manx'
+        }
+    });
+
+
+    var mailOptions = {
+        from: 'The Tech Guy <manny.navarro@gmail.com',
+        to: 'manny@emaginas.com',
+        subject: 'Website Submission',
+        text: 'You have a submission with the following details... Name: '+req.body.name+'Email: '+req.body.email+ 'Message: '+req.body.message,
+        html: '<p>You have a submission with the following details...</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+            res.redirect('/');
+        } else {
+            console.log('Message Sent: '+info.response);
+            res.redirect('/');
+        }
+    });
 });
 app.listen(2600);
 console.log('Server is running on port 2600...');
