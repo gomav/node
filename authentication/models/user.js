5995970e216bfed00558fc4f89a634bd1de5e804
Manny Navarro <manny.navarro@gmail.com>
2016-04-07 13:09:40 -0600

    added bcrypt.js to encrypt password, now passwords displays as a hash in the console and in the db

--- authentication/models/user.js
+++ authentication/models/user.js
@@ -1,5 +1,7 @@
 'use strict';
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
 
 mongoose.connect('mongodb://localhost/nodeauth');
 
@@ -28,5 +30,12 @@ var UserSchema = mongoose.Schema({
 var User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.createUser = function(newUser, callback){
-    newUser.save(callback);
+    var bcrypt = require('bcryptjs');
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(newUser.password, salt, function(err, hash) {
+            newUser.password = hash;
+            newUser.save(callback);
+        });
+    });
+
 };
