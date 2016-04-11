8b336dfbffd78eb50ef97d6eb2b9de159811ac4c
Manny Navarro <manny.navarro@gmail.com>
2016-04-11 12:13:28 -0600

    added necessary dependencies and middleware

--- nodeBlog/app.js
+++ nodeBlog/app.js
@@ -1,9 +1,18 @@
+'use strict';
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
+var moment = require('moment');
+var expressValidator = require('express-validator');
+
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeBlog');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +30,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Express session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+//Express Validator
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value){
+        var namespace = param.split('.'), root = namespace.shift(), formParam = root;
+
+        while(namespace.length){
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg  : msg,
+            value: value
+        };
+    }
+}));
+
+//Connect-Flash - for messaging(error)
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+//Make our db accessible to our router
+app.use(function(req, res, next){
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
